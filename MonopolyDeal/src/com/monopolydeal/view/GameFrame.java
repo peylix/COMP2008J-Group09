a1e3ed7f8a05bdf8918b914032f07c8c5a25428c
Jin <101871613+skjdsk@users.noreply.github.com>
2023-06-10 08:26:36 +0800

    Add files via upload

---
+++ MonopolyDeal/src/com/monopolydeal/view/GameFrame.java
@@ -0,0 +1,36 @@
+package com.monopolydeal.view;
+
+import com.monopolydeal.controller.GameController;
+import com.monopolydeal.view.screen.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+    public static final String TITLE = "Monopoly Deal";
+    public static final int WIDTH = 1030;
+    public static final int HEIGHT = 758;
+
+    private BaseScreen[] panels;
+
+    public GameFrame(GameController controller){
+        setTitle(TITLE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setResizable(false);
+        panels = new BaseScreen[4];
+        panels[0] = new MenuScreen(controller);
+        panels[1] = new CharacterScreen(controller);
+        panels[2] = new GameScreen(controller);
+        panels[3] = new WinScreen(controller);
+        changePanel(GameController.MENU);
+        setVisible(true);
+    }
+
+    public void changePanel(int index){
+        setContentPane(panels[index]);
+        panels[index].update();
+        pack();
+        setLocationRelativeTo(null);
+    }
+}
